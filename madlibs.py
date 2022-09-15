bc163962b5c198249f950e45d767e049a5d92102
Kevin Ramos <Kevramos93@yahoo.com>
2022-09-15 12:56:51 -0400

    add python file

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+# Program displays madlib given strings in different string concatenation formats
+
+name = "kevin"
+
+# Typical 
+print("Hello there " + name)
+
+# Using .format method
+print("Hello there {}".format(name))
+
+# Using f strings
+print(f"Hello there {name}")
